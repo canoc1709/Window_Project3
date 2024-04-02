1269556690abb558e72ed25f01b192faa64537cf
HAI DANG <HAI DANG@LAPTOP-RBPDT6R4>
2024-04-02 12:43:30 +0700

    Giao dien

--- Test/obj/Debug/Test_Content.g.i.cs
+++ Test/obj/Debug/Test_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("magnifier.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icons8-planet-100.png")]
 
 
